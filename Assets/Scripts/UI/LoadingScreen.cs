b00de370b0f78a8bd3ca89995f033d357305173b
alirezaRad <alirezababaee.a1380@gmail.com>
2025-11-19 21:19:23 -0800

    Create Loading Screen

---
+++ Assets/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+using System;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class LoadingScreen : MonoBehaviour
+    {
+        [Header("Main Root")]
+        [SerializeField] private CanvasGroup rootGroup;
+
+        [Header("Animated Parts")]
+        [SerializeField] private RectTransform topPart;
+        [SerializeField] private RectTransform bottomPart;
+
+        [Header("Loading UI")]
+        [SerializeField] private Slider fillBar;
+        [SerializeField] private TMP_Text progressText;
+
+        [Header("Animation Settings")]
+        [SerializeField] private float introDuration = 0.5f;
+        [SerializeField] private float fillDuration = 1f;
+        [SerializeField] private float outroDuration = 0.5f;
+        [SerializeField] private float moveOffset = 150f;
+
+        private Vector2 topStartPos;
+        private Vector2 bottomStartPos;
+
+        private void Start()
+        {
+            ShowLoadingBar(fillDuration, () =>
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            });
+        }
+
+
+        public void ShowLoadingBar(float fillTime, Action onFinished)
+        {
+            StartCoroutine(LoadingRoutine(fillTime, onFinished));
+        }
+
+
+        private IEnumerator LoadingRoutine(float fillTime, Action onFinished)
+        {
+            SetupInitialState();
+
+            DoIntro();
+            yield return new WaitForSeconds(introDuration);
+
+            yield return FillBarOverTime(fillTime);
+
+            DoOutro();
+            yield return new WaitForSeconds(outroDuration);
+
+            onFinished?.Invoke();
+        }
+
+
+        private void SetupInitialState()
+        {
+            fillBar.value = 0f;
+            progressText.text = "0%";
+
+            rootGroup.alpha = 0f;
+            
+            topStartPos = topPart.anchoredPosition;
+            bottomStartPos = bottomPart.anchoredPosition;
+            
+            topPart.anchoredPosition += new Vector2(0, moveOffset);
+            bottomPart.anchoredPosition -= new Vector2(0, moveOffset);
+        }
+
+
+        private void DoIntro()
+        {
+            rootGroup.DOFade(1f, introDuration);
+
+            topPart.DOAnchorPos(topStartPos, introDuration)
+                   .SetEase(Ease.OutCubic);
+
+            bottomPart.DOAnchorPos(bottomStartPos, introDuration)
+                      .SetEase(Ease.OutCubic);
+        }
+
+
+        private void DoOutro()
+        {
+            rootGroup.DOFade(0f, outroDuration);
+
+            topPart.DOAnchorPos(topStartPos + new Vector2(0, moveOffset), outroDuration)
+                   .SetEase(Ease.InCubic);
+
+            bottomPart.DOAnchorPos(bottomStartPos - new Vector2(0, moveOffset), outroDuration)
+                      .SetEase(Ease.InCubic);
+        }
+
+
+        private IEnumerator FillBarOverTime(float duration)
+        {
+            float t = 0f;
+
+            while (t < duration)
+            {
+                t += Time.deltaTime;
+                float progress = Mathf.Clamp01(t / duration);
+
+                fillBar.value = progress;
+                progressText.text = Mathf.RoundToInt(progress * 100f) + "%";
+
+                yield return null;
+            }
+
+            fillBar.value = 1f;
+            progressText.text = "100%";
+        }
+    }
+}
--- Assets/Scripts/UI/SplashScreen.cs
+++ Assets/Scripts/UI/SplashScreen.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 namespace UI
 {
@@ -11,33 +11,24 @@ namespace UI
         [SerializeField] private float fadeDuration = 1f;
         [SerializeField] private float splashDuration = 2f;
 
-        void Start()
+        private void Start()
         {
-            StartCoroutine(FadeInOut());
+            PlaySplashSequence();
         }
 
-        IEnumerator FadeInOut()
+        private void PlaySplashSequence()
         {
-            yield return StartCoroutine(Fade(0f, 1f));
-            yield return new WaitForSeconds(splashDuration);
-            yield return StartCoroutine(Fade(1f, 0f));
+            canvasGroup.alpha = 0f;
             
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
+            Sequence seq = DOTween.Sequence();
 
-        IEnumerator Fade(float startAlpha, float endAlpha)
-        {
-            float elapsedTime = 0f;
-            canvasGroup.alpha = startAlpha;
-            
-            while (elapsedTime < fadeDuration)
-            {
-                elapsedTime += Time.deltaTime;
-                canvasGroup.alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeDuration);
-                yield return null;
-            }
-            
-            canvasGroup.alpha = endAlpha;
+            seq.Append(canvasGroup.DOFade(1f, fadeDuration))      
+                .AppendInterval(splashDuration)                   
+                .Append(canvasGroup.DOFade(0f, fadeDuration))     
+                .OnComplete(() =>
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                });
         }
     }
 }
